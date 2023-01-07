a6ea52feadd5aa70ea9898336074a12d6be5962e
Romeo di Gregorio <romeo_digregorio@hotmail.com>
2023-01-07 11:18:55 +0100

    Add root controller

---
+++ SoftwareEngineerAssignment.Api/Controllers/RootController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoftwareEngineerAssignment.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace SoftwareEngineerAssignment.Api.Controllers;
+
+[ApiController]
+[Route("/")]
+public class RootController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetRoot()
+    {
+        GiveMeAdviceRequest giveMeAdviceRequest = new()
+        {
+            Topic = "car",
+            Amount = 2,
+        };
+
+        return Ok(new
+        {
+            Message = "Welcome to the Software Engineer Assignment API",
+            Endpoints = new
+            {
+                GiveMeAdvice = new
+                {
+                    Url = Url.Action(nameof(GiveMeAdviceController.GiveMeAdvice), "GiveMeAdvice"),
+                    Description = "Get a random advice slip setting a custom topic and total advice amount.",
+                    ExampleJSON = JsonSerializer.Serialize(
+                        giveMeAdviceRequest,
+                        new JsonSerializerOptions
+                        {
+                            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                        }),
+                },
+            }
+        });
+    }
+}
